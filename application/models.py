551d1142c2967c06b80298451d1129f9bec486a0
KarenNgala <ngalalalu@gmail.com>
2020-08-26 13:48:12 +0300

    Add user type model with boolean flags

--- application/models.py
+++ application/models.py
@@ -1,23 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class Profile(models.Model):
-    ''' extended User model '''
+
+class user_type( models.Model):
+    is_client = models.BooleanField(default=False)
+    is_doctor = models.BooleanField(default=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField(default='default.jpg', upload_to='avatars/')
-    bio = models.TextField(max_length=500, blank=True, default=f'Hello, I am new here!')
 
     def __str__(self):
-        return f'{self.user.username}'
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+        if self.is_client == True:
+            return self.user + " - is_client"
+        else:
+            return self.user + " - is_doctor"
