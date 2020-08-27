7df827e8a86e970372a2c18e7c315c86771b40d3
kangalah <nyongesajoannanjala@gmail.com>
2020-08-27 13:18:11 +0300

    create counsellor list-template

--- application/urls.py
+++ application/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$', views.home, name='home'),
+    url(r'^counsel/', views.counsel, name='counsel')
 ]
 
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) 
--- application/views.py
+++ application/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 
-# Create your views here.
+#Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def counsel(request):
+    return render(request, 'counsellors_list.html')
