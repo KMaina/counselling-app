0c33d7cf03ca7deb2e9db5e9340d942ed2798990
josphat-mwangi <josphatwanjiruw@gmail.com>
2020-08-27 13:17:58 +0300

    html structure

--- application/views.py
+++ application/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, redirect
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def counsellorhome(request):
+    return render(request, 'homecounsellor.html')
