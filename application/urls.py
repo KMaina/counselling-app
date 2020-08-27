85c62cf12ddec6f5df86f3b2b134c2a50b3f98a2
Mercy <aprilkasha42@gmail.com>
2020-08-27 13:40:37 +0300

    create client list template

--- application/urls.py
+++ application/urls.py
@@ -1,10 +1,11 @@
 from . import views
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$', views.home, name='home'),
+    url(r'^client_list/', views.client_list, name='client_list')
 ]
 
 if settings.DEBUG:
--- application/views.py
+++ application/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def client_list(request):
+    return render(request, 'client_list.html')
+
 def home(request):
     return render(request, 'index.html')
