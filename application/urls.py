90b4f9ba71a47dfb3c1e10a9a1bc1dee628770f2
KarenNgala <ngalalalu@gmail.com>
2020-09-08 11:36:56 +0300

    feature(ft-tests): Complete tests - Rewrite tests to ensure they all run - Add Disussion tests

--- application/urls.py
+++ application/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url,include
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
 
 
 urlpatterns=[
